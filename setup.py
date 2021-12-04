af1eb35beeb16731cde85bf4ebb2fde3acb32470
Gavin Lüdemann <gavin.luedemann@gmail.com>
2021-12-04 18:41:11 +0100

    Fix setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='merchantsguide',
+    version='0.1.0',
+    packages=['merchantsguide', 'merchantsguide.tests', 'merchantsguide.tests.end2end', 'merchantsguide.tests.unittests'],
+    url='',
+    license='MIT',
+    author='Gavin Lüdemann',
+    author_email='gavin.luedemann@gmail.com',
+    description='Merchan\'t Guide to the Galaxy'
+)

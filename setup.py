36ea62b7eda17e341bc2a7a3410150893d9c6330
Gavin Lüdemann <gavin.luedemann@gmail.com>
2021-12-05 12:00:57 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,14 @@ from setuptools import setup
 setup(
     name='merchantsguide',
     version='0.1.0',
-    packages=['merchantsguide', 'merchantsguide.tests', 'merchantsguide.tests.end2end', 'merchantsguide.tests.unittests'],
+    packages=['merchantsguide'],
     url='',
     license='MIT',
     author='Gavin Lüdemann',
     author_email='gavin.luedemann@gmail.com',
-    description='Merchan\'t Guide to the Galaxy'
+    description='Merchan\'t Guide to the Galaxy',
+    install_requires=[
+        'parsimonious~=0.8.1',
+        'singleton-decorator~=1.0.0'
+    ]
 )
